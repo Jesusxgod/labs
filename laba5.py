e91e60ef71a24929b6a21e031b8734a980496f24
Danila <legionerddno@yandex.com>
2022-10-07 18:49:04 +0800

    Update laba5.py

--- laba5.py
+++ laba5.py
@@ -1,4 +1,19 @@
 ######### 1 #########
+# zip()
+a = [1,2,3]
+b = "xyz"
+c = (None, True)
+
+res = list(zip(a, b, c))
+print (res)
+
+# reduce()
+from functools import reduce
+items = [1, 24, 17, 14, 9, 32, 2]
+all_max = reduce(lambda a,b: a if (a > b) else b, items)
+
+print (all_max)
+
 
 ######### 2 #########
 # Вариант 10
@@ -22,3 +37,30 @@ import math
 def f(x: float) -> float:
     return math.sqrt(x) - math.cos(0.387*x)
 print(f"y = {f(float(input('x = ')))}")
+
+######### 3 #########
+# Вариант 10 
+# херня полная
+import math
+
+def f(x, y: float) -> float:
+    return math.sin(y-1) + x - 1.3
+    return y - math.sin(x + 1) - 0.8
+print(f"y = {f(float(input('x = ')))}")
+
+######### 3 #########
+# Вариант 10
+# доработат
+a = int(input("Введите число 1:"))
+b = int(input("Введите число 2:"))
+temp1 = a
+temp2 = ab
+rev = 0
+while(n > 0):
+    dig = a % 10
+    rev = rev * 10 + dig
+    a = a // 10
+if(temp == rev):
+    print("Это палиндром!")
+else:
+    print("Это не палиндром!")
